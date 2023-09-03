70562baf19c00c1fe70a91f5cc5ccd33fee3fc98
georgeikani <ikanigeorge2@.com>
2023-09-03 14:14:24 +0100

    CRUD entry and transfer

--- simplebank/db/migration/000001_init_schema.up.sql
+++ simplebank/db/migration/000001_init_schema.up.sql
@@ -8,33 +8,33 @@ CREATE TABLE "accounts" (
 
 CREATE TABLE "entries" (
   "id" bigserial PRIMARY KEY,
-  "Account_id" bigint NOT NULL,
-  "Amount" bigint NOT NULL,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
   "id" bigserial PRIMARY KEY,
-  "from_Account_id" bigint NOT NULL,
-  "to_Account_id" bigint NOT NULL,
-  "Amount" bigint NOT NULL,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE INDEX ON "accounts" ("owner");
 
-CREATE INDEX ON "transfers" ("from_Account_id");
+CREATE INDEX ON "transfers" ("from_account_id");
 
-CREATE INDEX ON "transfers" ("to_Account_id");
+CREATE INDEX ON "transfers" ("to_account_id");
 
-CREATE INDEX ON "transfers" ("from_Account_id", "to_Account_id");
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 
-COMMENT ON COLUMN "entries"."Amount" IS 'can be negative or positive';
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
 
-COMMENT ON COLUMN "transfers"."Amount" IS 'must positive or negative';
+COMMENT ON COLUMN "transfers"."amount" IS 'must positive or negative';
 
-ALTER TABLE "entries" ADD FOREIGN KEY ("Account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
-ALTER TABLE "transfers" ADD FOREIGN KEY ("from_Account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
 
-ALTER TABLE "transfers" ADD FOREIGN KEY ("to_Account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
